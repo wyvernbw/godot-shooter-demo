99e8f566588398d2ace34f1f796af9175e44c208
wyvernbw <iamwyvern511@gmail.com>
2022-03-25 22:37:41 +0200

    Fix joystick bug

--- gui/joystick.gd
+++ gui/joystick.gd
@@ -1,18 +1,20 @@
+tool
 extends Node2D
 
 signal joystick_moved(dir)
 
 export var available_radius : int = 32
 export var button_radius : int = 8
+export var search_radius : int = 128 setget set_search_radius
 
 onready var button := get_node("TouchScreenButton")
 
-var in_search_area : bool = false
 var joystick_active : bool = false
+
 
 func _input(event : InputEvent) -> void:
 	if event is InputEventScreenTouch or event is InputEventScreenDrag:
-		if in_search_area or joystick_active:
+		if in_search_area(event.position) or joystick_active:
 			button.global_position = event.position - Vector2(button_radius, button_radius)
 			joystick_active = true
 		if event is InputEventScreenTouch:
@@ -27,15 +29,18 @@ func _input(event : InputEvent) -> void:
 func _process(_delta) -> void:
 	if joystick_active:
 		emit_signal("joystick_moved", calculate_move_vector())
-
+	if Engine.editor_hint:
+		self.search_radius = search_radius
 
 func calculate_move_vector() -> Vector2:
 	return (button.global_position - self.position).normalized()
 
+func in_search_area(pos : Vector2) -> bool:
+	return pos.length() <= search_radius
 
-func _on_SearchZone_mouse_entered():
-	in_search_area = true
-
-
-func _on_SearchZone_mouse_exited():
-	in_search_area = false
+func set_search_radius(new_radius : int) -> void:
+	search_radius = new_radius
+	if not get_node_or_null("SearchAreaPreview"):
+		return
+	$SearchAreaPreview.scale.x = float(new_radius) / 32 
+	$SearchAreaPreview.scale.y = float(new_radius) / 32 
